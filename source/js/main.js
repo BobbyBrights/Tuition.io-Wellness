04b6f4bcdc1132636774f35b6f62d2fb39a32fb6
Craig Fowler <craig@howler.la>
2017-12-07 18:02:44 -0800

    things

---
+++ source/js/main.js
@@ -0,0 +1,7 @@
+// @codekit-prepend "vendors.js"
+
+$(document).ready(function(){
+  $('.lightbox').lightbox();
+  $('.swap').swap();
+  //$(".sticky").sticky();
+});
---
+++ source/js/vendors.js
@@ -0,0 +1,10 @@
+// Formstone Components
+// @codekit-prepend "../bower_components/formstone/dist/js/core.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/touch.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/transition.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/analytics.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/mediaquery.js"
+
+// @codekit-prepend "../bower_components/formstone/dist/js/swap.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/lightbox.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/sticky.js"
